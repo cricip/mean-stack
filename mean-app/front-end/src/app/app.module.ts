571317fa4d2c30672c056102d6b042d0ff9030b6
Cristina Cipcigan <cristinacipcigan@gmail.com>
2019-10-10 14:16:28 +0300

    Task service

--- mean-app/front-end/src/app/app.module.ts
+++ mean-app/front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { MatToolbarModule } from '@angular/material';
 
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { EditComponent } from './components/edit/edit.component';
 import { CreateComponent } from './components/create/create.component';
+import { TaskService } from './task.service';
 
 const routes: Routes = [
   { path: 'create', component: CreateComponent},
@@ -29,10 +31,11 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
     MatToolbarModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
