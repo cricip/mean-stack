821b6eeb9a58508797b2597ccd737636b390b6f1
Cristina Cipcigan <cristinacipcigan@gmail.com>
2019-10-11 19:49:31 +0300

    Create Component - form

--- mean-app/front-end/src/app/components/create/create.component.ts
+++ mean-app/front-end/src/app/components/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router'
 import { TaskService } from '../../task.service';
 
 @Component({
@@ -6,9 +8,25 @@ import { TaskService } from '../../task.service';
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.sass']
 })
+
 export class CreateComponent implements OnInit {
 
-  constructor(private taskService: TaskService) { }
+  createForm: FormGroup;
+
+  constructor(private taskService: TaskService, private fb: FormBuilder, private router: Router) {
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      difficulty: ''
+    });
+  }
+
+  addTask(title, responsible, description, difficulty) {
+    this.taskService.addTask(title, responsible, description, difficulty).subscribe(() => {
+      this.router.navigate(['/list']);
+    });
+  }
 
   ngOnInit() {
   }
